031e8ee8a6bdf811e1fdc15227e4bf5d75070c61
myriamleb9 <lebbakherm@gmail.com>
2025-07-18 14:42:47 -0700

    Changes EX3 & 4

---
+++ 3-value_argument.js
@@ -0,0 +1,1 @@
+console.log(process.argv[2] === undefined ? "No argument" : process.argv[2]);
---
+++ 4-concat.js
@@ -0,0 +1,1 @@
+const arg1 = process.argv[2]; const arg2 = process.argv[3]; console.log(`${arg1} is ${arg2}`);
